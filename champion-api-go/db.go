d01706dcb9509466cae13c5729c59612d7c45500
RomainPellegrini <romain.83.rp@gmail.com>
2026-04-14 15:27:37 +0200

    feat : ajout du service Go

---
+++ champion-api-go/db.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	// Charger le .env (chemin relatif)
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Erreur chargement .env")
+	}
+
+	// Récupérer les variables
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+	host := "localhost"
+	port := "5432"
+
+	// Construire la string de connexion
+	connStr := fmt.Sprintf(
+		"user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		user, password, dbname, host, port,
+	)
+
+	var errDB error
+	DB, errDB = sql.Open("postgres", connStr)
+	if errDB != nil {
+		log.Fatal(errDB)
+	}
+
+	errDB = DB.Ping()
+	if errDB != nil {
+		log.Fatal(errDB)
+	}
+
+	fmt.Println("✅ Connected to PostgreSQL")
+}
